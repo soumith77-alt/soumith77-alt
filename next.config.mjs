67e7245367676d2d9744d78936eb92102c0e6256
Keel <soumith77@gmail.com>
2025-11-05 21:21:49 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: 'export',           // 👈 this tells Next to make /out
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
 }
 
 export default nextConfig
+
